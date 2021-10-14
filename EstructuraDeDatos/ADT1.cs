128df58cca3da96ac6105846262258b17b5bb208
Kevin Perez Sesma <a20311111@uthermosillo.edu.mx>
2021-10-13 21:35:55 -0700

    Fibonnaci code
    Signed-off-by: Kevin Perez Sesma <a20311111@uthermosillo.edu.mx>

--- EstructuraDeDatos/ADT1.cs
+++ EstructuraDeDatos/ADT1.cs
@@ -8,7 +8,12 @@ namespace EstructuraDeDatos
 {
     class ADT1
     {
-        public string dato1;
-        public string dato2;
+            public string propiedad_1;
+            public string propiedad_2;
+        public ADT1(String Propiedad_1, String Propiedad_2)
+        {
+            propiedad_1 = Propiedad_1;
+            propiedad_2 = Propiedad_2;
+        }
     }
 }
